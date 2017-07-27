ea57b944ced1b62a8f9511a6638ec525c6c493f6
at-dungvan <dungvan2512@gmail.com>
2017-07-27 17:53:42 +0700

    update folder structure

---
+++ database/migrations/2017_07_27_083733_create_food_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFoodTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->float('price',10,2);
+            $table->text('description');
+            $table->string('image');
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food');
+    }
+}
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Food-market-order</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+    @include('layouts.partials.style')
+
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+    <!-- Header -->
+    @include('layouts.partials.header')
+    <!-- Left side column. contains the logo and sidebar -->
+    @include('layouts.partials.aside')
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        @include('layouts.partials.main-header')
+
+        <!-- Main content -->
+        <section class="content">
+
+            <!-- Your Page Content Here -->
+            @yield('main-content')
+
+        </section>
+        <!-- /.content -->
+
+    </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Main Footer -->
+    @include('layouts.partials.footer')
+
+    <!-- Add the sidebar's background. This div must be placed
+         immediately after the control sidebar -->
+    <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED JS SCRIPTS -->
+
+@include('layouts.partials.script')
+@yield('main-js')
+
+</body>
+</html>
