3ecfbe463c519e0aa23e9973c952b0b9a055face
at-vudang <ngocvupct1995@gmail.com>
2017-08-05 20:14:52 +0700

    Update admin edit category v1.0

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,5 +6,33 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id It is id of category need update
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        return view('categories.edit', ['category' => $category]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request Request from client
+     * @param int                      $id      It is id of category need update
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+        $category->name = $request->categoryName;
+        $category->description = $request->description;
+        $category->save();
+        return view('categories.edit', ['category'=> $category]);
+    }
 }
