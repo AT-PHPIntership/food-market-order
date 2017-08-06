6ee417a334bc4ead04f30e3e5492c916eb193596
at-dungvan <dungvan2512@gmail.com>
2017-08-06 22:04:45 +0700

    Update 2017_07_28_175424_create_orders_table.php

--- database/migrations/2017_07_28_175424_create_orders_table.php
+++ database/migrations/2017_07_28_175424_create_orders_table.php
@@ -15,7 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned()->nullable();
+            $table->integer('user_id')->unsigned();
             $table->datetime('trans_at');
             $table->text('custom_address');
             $table->timestamps();
