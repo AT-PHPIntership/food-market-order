8365e114b933c043815963306a11176ae78a6d4b
at-dungvan <dungvan2512@gmail.com>
2017-08-06 22:42:57 +0700

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,9 @@
 <?php
+
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+
 class CreateUsersTable extends Migration
 {
     /**
@@ -20,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->timestamps();
         });
     }
+    
     /**
      * Reverse the migrations.
      *
