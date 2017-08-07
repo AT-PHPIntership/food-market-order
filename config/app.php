192620734a1e95e13b9fe350c1dcd7986cf36c81
at-dungvan <dungvan2512@gmail.com>
2017-08-07 17:58:04 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'foodmarket.com'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
