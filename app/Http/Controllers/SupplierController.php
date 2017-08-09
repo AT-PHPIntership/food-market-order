1416ef397c8bc412552236637fbf7a115f71a926
at-vudang <ngocvupct1995@gmail.com>
2017-08-09 16:43:45 +0700

    Create Supplier v 1.0

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SupplierRequest;
 use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    //
+    /**
+     * Variable common object Supplier
+     *
+     * @var Supplier $Supplier
+     */
+    private $supplier;
+    /**
+     * SupplierController constructor.
+     *
+     * @param Supplier $Supplier It is param input constructors
+     */
+    public function __construct(Supplier $supplier)
+    {
+        $this->supplier = $supplier;
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('categories.create');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request Request from client
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(SupplierRequest $request)
+    {
+        $this->supplier->create($request->all());
+        return redirect()->route('categories.index');
+    }
 }
