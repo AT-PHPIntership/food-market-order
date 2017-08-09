2d167d4694cf7c80386839d69dee8a2f7917db6f
at-vudang <ngocvupct1995@gmail.com>
2017-08-09 15:10:40 +0700

    Update Supplier v1.2

---
+++ app/Http/Requests/SupplierRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SupplierRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+         return [
+           'name' => 'required|unique:suppliers|max:255',
+         ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => ' The supplier name field is required.',
+            'name.max' => ' The supplier name may not be greater than 255 characters.',
+            'name.unique' => ' The supplier name is existed.',
+        ];
+    }
+}
