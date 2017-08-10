861f5170cabdc5b41701fdddfcb084b821807f86
at-dungvan <dungvan2512@gmail.com>
2017-08-10 12:03:28 +0700

    update flash service provider -  fix login - update test browser

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(".btn-confirm-delete").bind('click',function(){
-        var result = confirm($(".btn-confirm-delete").attr("data-confirm"));
-        if(result){
-            $('form.delete-item').submit();
-        }
-    });
-});
