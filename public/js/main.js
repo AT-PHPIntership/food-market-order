dd84b960553175b7232437bbbdb96a1f92816e97
at-vudang <ngocvupct1995@gmail.com>
2017-08-14 13:55:39 +0700

    Update fix comment of trainer

--- public/js/main.js
+++ public/js/main.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
     $(".btn-confirm").bind('click',function(e){
         e.preventDefault();
         var form = $(this.form);
-        var title = $(".btn-confirm").attr("data-title");
-        var content = '<p>'+$(".btn-confirm").attr("data-confirm")+'</p>'
-        $('#myModalTitle').html(title);
-        $('#myModalBody').html(content);
-        $('#myModal').modal('show');
-        $("#btnSend").one("click", function(){
+        var title = $(this).attr("data-title");
+        var body = '<p>'+$(this).attr("data-confirm")+'</p>'
+        $('#modal-confirm-title').html(title);
+        $('#modal-confirm-body').html(body);
+        $('#modal-confirm').modal('show');
+        $("#btn-modal-submit").one("click", function(){
             form.submit();
-            $('#myModal').modal('hide');
+            $('#modal-confirm').modal('hide');
         })
     });
     $("#date-sort").change(function(){
