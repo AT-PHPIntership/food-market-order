04f3227343d52e0f5ca88f230cbb31e69be44294
at-dungvan <dungvan2512@gmail.com>
2017-08-16 16:34:02 +0700

    v1.o0

---
+++ tests/Browser/AdminUpdateUserTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class AdminUpdateUserTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+}
