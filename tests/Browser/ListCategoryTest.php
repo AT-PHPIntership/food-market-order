ff9bd714e41995af7113ddb4fda144f14352f584
at-vudang <ngocvupct1995@gmail.com>
2017-08-16 16:04:38 +0700

    Test List Category

--- tests/Browser/ListCategoryTest.php
+++ tests/Browser/ListCategoryTest.php
@@ -13,6 +13,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ListCategoryTest extends DuskTestCase
 {
+
     /**
      * A Dusk test example.
      *
@@ -28,7 +29,60 @@ class ListCategoryTest extends DuskTestCase
     }
 
     /**
-     * A Dusk test example.
+     * Test data empty.
+     *
+     * @return void
+     */
+    public function testDataEmpty()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                ->visit('/categories')
+                ->assertSee('List Categories')
+            ->screenshot('sdsd');
+            $elements = $browser->elements('.dataTable tbody tr');
+            $numRecord = count($elements);
+            $this->assertTrue($numRecord == 0);
+        });
+    }
+
+    /**
+     * Test pagination.
+     *
+     * @return void
+     */
+    public function testPagination()
+    {
+        $this->browse(function (Browser $browser) {
+            // Test data have 10 record
+            factory(Category::class,10)->create();
+            $elements = $browser->visit('/categories')
+                ->elements('.dataTable tbody tr');
+            $numRecord = count($elements);
+            $this->assertTrue($numRecord == 10);
+
+            // Test data have 12 record
+            factory(Category::class,2)->create();
+            // Page 1 table have 10 record
+            $elements = $browser->visit('/categories')
+                ->elements('.dataTable tbody tr');
+            $numRecord = count($elements);
+            $this->assertTrue($numRecord == 10);
+            // Page 2 table have 2 record
+            $elements = $browser->visit('/categories?page=2')
+                ->elements('.dataTable tbody tr');
+            $numRecord = count($elements);
+            $this->assertTrue($numRecord == 2);
+            // Page 3 not access
+            $elements = $browser->visit('/categories?page=3')
+                ->elements('.dataTable tbody tr');
+            $numRecord = count($elements);
+            $this->assertTrue($numRecord == 0);
+        });
+    }
+
+    /**
+     * Test button click Add Category.
      *
      * @return void
      */
@@ -42,23 +96,43 @@ class ListCategoryTest extends DuskTestCase
     }
 
     /**
-     * A Dusk test example.
+     * Test button click Edit Category.
      *
      * @return void
      */
-    public function testClickDelete()
+    public function testClickEdit()
     {
+        factory(Category::class, 10)->create();
         $this->browse(function (Browser $browser) {
             // Click button add category
             $element = 'tbody tr:nth-child(2)';
             $id = $browser->visit('/categories')
                 ->text($element. ' td');
-            echo $id;
+
+            $browser->visit('/categories')
+                ->click($element. ' .fa-edit')
+                ->assertPathIs('/categories/'.$id.'/edit')->screenshot('abc2');
+        });
+    }
+
+
+    /**
+     * Test button click Add Category.
+     *
+     * @return void
+     */
+    public function testClickDelete()
+    {
+        factory(Category::class, 10)->create();
+        $this->browse(function (Browser $browser) {
+            // Click button add category
+            $element = 'tbody tr:nth-child(2)';
+            $id = $browser->visit('/categories')
+                ->text($element. ' td');
             $browser->visit('/categories')
                 ->click($element. ' .fa-trash')
-                ->assertSee('Delete Category')
-                ->screenshot('s')
-                ->assertPathIs('/categories');
+                ->waitFor(null,3)
+                ->assertSeeIn('#modal-confirm-title','Delete Category');
         });
     }
 }
--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,6 +3,8 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 trait CreatesApplication
 {
@@ -18,5 +20,28 @@ trait CreatesApplication
         $app->make(Kernel::class)->bootstrap();
 
         return $app;
+    }
+
+    /**
+     * This functin is called before testcase
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        Artisan::call('migrate');
+        DB::table('users')->insert([
+            'full_name' => 'DungVan',
+            'email' => 'admin' . '@gmail.com',
+            'password' => bcrypt('123456'),
+            'is_admin' => 1,
+        ]);
+    }
+    /**
+     * This function is called after testcase
+     */
+    public function tearDown()
+    {
+        Artisan::call('migrate:rollback');
+        parent::tearDown();
     }
 }
