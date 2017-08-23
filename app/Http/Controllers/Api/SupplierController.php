c0600525957d29e1c1810a25203020aa0f501235
at-dungvan <dungvan2512@gmail.com>
2017-08-23 16:54:12 +0700

    update template api

---
+++ app/Http/Controllers/Api/SupplierController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SupplierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request request create
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request = $request;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id id supplier
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $id = $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param int $id id supplier
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $id = $id;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request request update
+     * @param int                      $id      id supplier update
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request = $request;
+        $id = $id;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id id delete
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $id = $id;
+    }
+}
