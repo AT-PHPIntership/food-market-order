1d80666843553b6054bee966caa4e2bf386a69ce
at-dungvan <dungvan2512@gmail.com>
2017-09-08 10:37:02 +0700

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,16 +7,16 @@
 <div class="error-page">
     <h2 class="headline text-yellow"> 404</h2>
 
-    <div class="error-content">
-        <h3><i class="fa fa-warning text-yellow"></i> {{ __('Oops! Page not found.') }}</h3>
+        <div class="error-content">
+            <h3><i class="fa fa-warning text-yellow"></i> {{ __('Oops! Page not found.') }}</h3>
 
-        <p>
-            {{ __('We could not find the page you were looking for.
-            Meanwhile, you may') }} <a href="{{ route('dashboard')  }}"> {{ __('return to dashboard') }}</a>
-        </p>
+            <p>
+                {{ __('We could not find the page you were looking for.
+                Meanwhile, you may') }} <a href="{{ route('dashboard')  }}"> {{ __('return to dashboard') }}</a>
+            </p>
 
+        </div>
+        <!-- /.error-content -->
     </div>
-    <!-- /.error-content -->
-</div>
 </body>
 </html>
