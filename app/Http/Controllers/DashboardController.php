d94e7dd3b277abfd5aea62c5e3a9cb39447d07e4
hungnguyen <hungbkdn1295@gmail.com>
2017-09-12 13:46:37 +0700

    Add statistics for dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,16 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\DailyMenu;
+use App\Material;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
+    protected $curDate;
+
     /**
      * Create a new controller instance.
      *
+     * @param string $date current date
+     *
      * @return void
      */
-    public function __construct()
+    public function __construct(Carbon $carbon)
     {
+        $carbon = $carbon->today();
+        $format = $carbon->format('Y-m-d');
+        $this->curDate = $format;
+    }
+
+    /**
+     * Get total of new order, register, ... on day
+     *
+     * @param string $modelType type of model
+     *
+     * @return int
+     */
+    public function getTotalOf($modelType)
+    {
+        $total = $modelType::where('created_at', 'like', '%'.$this->curDate.'%')->count();
+        return $total;
+    }
+
+    /**
+     * Get total of new register on day
+     *
+     * @return Integer
+     */
+    public function getStatistics()
+    {
+        $statistics = new \stdClass();
+        $statistics->totalOrder = $this->getTotalOf('App\\Order');
+        $statistics->totalRegister = $this->getTotalOf('App\\User');
+        $statistics->totalFood = DailyMenu::where('date', '=', $this->curDate)->count();
+        $statistics->totalMaterial = Material::count();
+        $statistics->date = $this->curDate;
+
+        return $statistics;
     }
 
     /**
@@ -22,6 +62,8 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $statistics = $this->getStatistics();
+
+        return view('dashboard', ['statistics' => $statistics]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,31 +5,30 @@
     <div class="row">
         <div class="col-lg-3 col-xs-6">
             <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>{{ $statistics->totalRegister }}</h3>
+
+                    <p>{{ __('User Registrations') }}</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-person-add"></i>
+                </div>
+                <a href="{{ route('users.index') }}" class="small-box-footer">{{ __('More info') }}<i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
             <div class="small-box bg-aqua">
                 <div class="inner">
-                    <h3>150</h3>
+                    <h3>{{ $statistics->totalOrder }}</h3>
 
                     <p>{{ _('New Orders') }}</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">{{ __('More info') }}<i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-green">
-                <div class="inner">
-                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                    <p>{{ __('Bounce Rate') }}</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-stats-bars"></i>
-                </div>
-                <a href="#" class="small-box-footer">{{ __('More info') }}<i class="fa fa-arrow-circle-right"></i></a>
+                <a href="{{ route('orders.index') }}" class="small-box-footer">{{ __('More info') }}<i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
@@ -37,14 +36,14 @@
             <!-- small box -->
             <div class="small-box bg-yellow">
                 <div class="inner">
-                    <h3>44</h3>
+                    <h3>{{ $statistics->totalMaterial }}</h3>
 
-                    <p>{{ __('User Registrations') }}</p>
+                    <p>{{ __('Materials') }}</p>
                 </div>
                 <div class="icon">
-                    <i class="ion ion-person-add"></i>
+                    <i class="ion ion-ios-rose"></i>
                 </div>
-                <a href="#" class="small-box-footer">{{ __('More info') }}<i class="fa fa-arrow-circle-right"></i></a>
+                <a href="{{ route('materials.index') }}" class="small-box-footer">{{ __('More info') }}<i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
@@ -52,14 +51,14 @@
             <!-- small box -->
             <div class="small-box bg-red">
                 <div class="inner">
-                    <h3>65</h3>
+                    <h3>{{ $statistics->totalFood }}</h3>
 
-                    <p>{{ __('Unique Visitors') }}</p>
+                    <p>{{ __('Daily Menu') }}</p>
                 </div>
                 <div class="icon">
-                    <i class="ion ion-pie-graph"></i>
+                    <i class="ion ion-ios-list"></i>
                 </div>
-                <a href="#" class="small-box-footer">{{ __('More info') }}<i class="fa fa-arrow-circle-right"></i></a>
+                <a href="{{ route('daily-menus.show', $statistics->date) }}" class="small-box-footer">{{ __('More info') }}<i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
         <!-- ./col -->
