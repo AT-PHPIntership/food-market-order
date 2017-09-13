652737433841c91a10fc5056d0bd2ebeeb148265
hungnguyen <hungbkdn1295@gmail.com>
2017-09-13 11:05:54 +0700

    --amen

--- app/Http/Controllers/Api/StatisticController.php
+++ app/Http/Controllers/Api/StatisticController.php
@@ -8,63 +8,50 @@ use App\Http\Controllers\Controller;
 use App\Material;
 use App\Food;
 use App\OrderItem;
+use App\Order;
+use App\Category;
+use App\DailyMenu;
+use App\User;
+use App\Supplier;
 
 class StatisticController extends ApiController
 {
     /**
-     * Get count of resources.
+     * The OrderItem implementation.
+     *
+     * @var OrderItem
+     */
+    protected $orderItem;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param OrderItem $orderItem instance of OrderItem
+     *
+     * @return void
+     */
+    public function __construct(OrderItem $orderItem)
+    {
+        $this->orderItem = $orderItem;
+    }
+
+    /**
+     * Get count of category, dailymenu, food, material, order, supplier, user.
      *
      * @return \Illuminate\Http\Response
      */
     public function countResources()
     {
-        $modelFiles = \File::files('../app/');
-
-        foreach ($modelFiles as $modelFile) {
-            $fileName = \File::name($modelFile);
-            if ($fileName == 'OrderItem') {
-                continue;
-            }
-            $modelName = 'App\\'.$fileName;
-            $modelInstance = new $modelName;
-            if ($fileName == 'DailyMenu') {
-                $curDate = date('Y-m-d');
-                $counts[$modelInstance->getTable()] = $modelInstance->where('date', '=', $curDate)->count();
-                continue;
-            }
-            $counts[$modelInstance->getTable()] = $modelInstance->count();
-        }
+        $curDate = date('Y-m-d');
+        $counts['categories'] = Category::count();
+        $counts['daily-menus'] = DailyMenu::where('date', '=', $curDate)->count();
+        $counts['foods'] = Food::count();
+        $counts['materials'] = Material::count();
+        $counts['orders'] = Order::count();
+        $counts['suppliers'] = Supplier::count();
+        $counts['users'] = User::count();
 
         return response()->json(collect(['data' => $counts])->merge(['success' => true]));
-    }
-
-    /**
-     * Get top of order by table type
-     *
-     * @param string $tableType type of target table
-     *
-     * @return array
-     */
-    public function getTopOrderOf($tableType)
-    {
-        $modelInstance = new $tableType;
-        $tableName = $modelInstance->getTable();
-        $columns = [
-            $tableName.'.id',
-            $tableName.'.name',
-            $tableName.'.category_id',
-            $tableName.'.price',
-            $tableName.'.image',
-            $tableName.'.description',
-            'count(*) as total_order'
-        ];
-        return OrderItem::selectRaw(implode(' ,', $columns))
-                        ->join($tableName, $tableName.'.id', '=', 'itemtable_id')
-                        ->groupBy(['itemtable_id', 'itemtable_type'])
-                        ->where('itemtable_type', $tableType)
-                        ->orderBy('total_order', 'desc')
-                        ->take(5)
-                        ->get();
     }
 
     /**
@@ -74,14 +61,14 @@ class StatisticController extends ApiController
      */
     public function getTrends()
     {
-        $topFoods = $this->getTopOrderOf('App\\Food');
-        $topMaterials = $this->getTopOrderOf('App\\Material');
+        $topFoods = $this->orderItem->getTrendOrderByModel('App\\Food');
+        $topMaterials = $this->orderItem->getTrendOrderByModel('App\\Material');
 
-        if ($topFoods && $topMaterials) {
+        if ($topFoods || $topMaterials) {
             return response()->json(collect([
                         'data' => [
-                            'foods' => $topFoods,
-                            'materials' => $topMaterials
+                            'foods' => ($topFoods ? $topFoods : ''),
+                            'materials' => ($topMaterials ? $topMaterials : '')
                         ]])->merge(['success' => true]));
         }
         $error = __('Has error during access this page');
